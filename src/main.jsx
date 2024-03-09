741b2d008e69c0a9273519548bc2a708b77fd702
Anton <anton7uzunov7@gmail.com>
2024-03-09 13:58:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,5 @@ import  "@fontsource/outfit";
 import  "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
